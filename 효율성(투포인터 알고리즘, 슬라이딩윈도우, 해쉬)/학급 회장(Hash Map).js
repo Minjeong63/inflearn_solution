f413565aa3d01e6cdb9033a02f06111a61213c53
Minjeong63 <kmj06030@naver.com>
2023-04-05 18:03:36 +0900

    a

--- 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/학급 회장(Hash Map).js
+++ 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/학급 회장(Hash Map).js
@@ -25,6 +25,18 @@ function solution(str) {
  */
 function solution1(str) {
   let answer;
+  let sH = new Map();
+  for (let x of str) {
+    if (sH.has(x)) sH.set(x, sH.get(x) + 1);
+    else sH.set(x, 1);
+  }
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let [key, val] of sH) {
+    if (val > max) {
+      max = val;
+      answer = key;
+    }
+  }
   return answer;
 }
-// console.log(solution("BACBACCACCBDEDE"));
+// console.log(solution1("BACBACCACCBDEDE"));
