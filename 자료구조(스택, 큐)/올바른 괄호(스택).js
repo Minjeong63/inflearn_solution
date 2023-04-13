93a55bec9cf78ff4c1d00a31e87360edc565a98e
Minjeong63 <kmj06030@naver.com>
2023-04-13 18:19:25 +0900

    a

---
+++ 자료구조(스택, 큐)/올바른 괄호(스택).js
@@ -0,0 +1,18 @@
+/**
+ * 1번
+ * 괄호가 입력되면 올바른 괄호이면 "YES", 올바르지 않으면 "NO"를 출력합니다.
+ * (())() 이것은 괄호의 쌍이 올바르게 위치하는 거지만, (()()))은 올바른 괄호가 아니다.
+ *
+ */
+function solution(n) {
+  let answer;
+  return answer;
+}
+
+/**
+ * 강의 해결법
+ */
+function solution1(n) {
+  let answer;
+  return answer;
+}
