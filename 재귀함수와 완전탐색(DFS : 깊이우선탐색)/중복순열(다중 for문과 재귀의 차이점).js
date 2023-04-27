c2900284d204b79e29e0b1c22b58cbd90c3e00ac
Minjeong63 <kmj06030@naver.com>
2023-04-27 17:05:15 +0900

    2023/04/27

--- 재귀함수와 완전탐색(DFS : 깊이우선탐색)/중복순열(다중 for문과 재귀의 차이점).js
+++ 재귀함수와 완전탐색(DFS : 깊이우선탐색)/중복순열(다중 for문과 재귀의 차이점).js
@@ -2,16 +2,15 @@
  * 8번
  * 1부터 N까지 번호가 적힌 구슬이 있습니다.
  * 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열 하는 방법을 모두 출력합니다.
+ * 강의 해결법
  * X
  */
 function solution(n, m) {
   let answer = [];
-  let tmp = [];
+  let tmp = Array.from({ length: m }, () => 0);
   function DFS(L) {
     if (L === m) {
-      console.log("tmp ", tmp, L);
-      answer.push(tmp);
-      console.log(tmp, L);
+      answer.push(tmp.slice());
     } else {
       for (let i = 1; i <= n; i++) {
         tmp[L] = i;
@@ -22,12 +21,4 @@ function solution(n, m) {
   DFS(0);
   return tmp;
 }
-
-/**
- * 강의 해결법
- */
-function solution1(n, m) {
-  let answer;
-  return answer;
-}
-console.log(solution(3, 2));
+// console.log(solution(3, 2));
