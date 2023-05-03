54143a9388ee7af49ee8425bfaca66771d82d1fb
Minjeong63 <kmj06030@naver.com>
2023-05-03 17:06:02 +0900

    2023/05/03

--- 재귀함수와 완전탐색(DFS : 깊이우선탐색)/수들의 조합.js
+++ 재귀함수와 완전탐색(DFS : 깊이우선탐색)/수들의 조합.js
@@ -4,19 +4,22 @@
  * 몇 개가 있는지 출력하는 프로그램을 작성하세요.
  * 예를 들면 5개의 숫자 2 4 5 8 12가 주어지고,
  * 3개를 뽑은 조합의 합이 6의 배수인 조합을 찾으면 4+8+12 2+4+12로 2가지가 있습니다.
- *
+ * O
  */
 function solution(arr, k, m) {
   let answer = 0;
-  function DFS(L, sum) {
+  arr.sort((a, b) => a - b);
+  function DFS(L, sum, s) {
     if (L === k) {
-      if (sum % 6 === 0) answer++;
-      else return;
+      if (sum % m === 0) answer++;
     } else {
-      for (let i = 0; i < arr.length; i++) {}
+      for (let i = s; i < arr.length; i++) {
+        DFS(L + 1, sum + arr[i], i + 1);
+      }
     }
+    console.log(L, "1234");
   }
-  DFS(0, 0);
+  DFS(0, 0, 0);
   return answer;
 }
 
@@ -27,4 +30,4 @@ function solution1(arr, k, m) {
   let answer;
   return answer;
 }
-console.log(solution([2, 4, 5, 8, 12], 3, 6));
+// console.log(solution([2, 4, 5, 8, 12], 3, 6));
